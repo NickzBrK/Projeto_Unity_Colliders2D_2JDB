e4b1c64b92ae47f9a70a66030a4c707338f89958
GlauberLP <glauber153hp@gmail.com>
2026-04-09 10:40:34 -0300

    Update projeto | Add flip

--- Assets/Script/Movimento.cs
+++ Assets/Script/Movimento.cs
@@ -2,19 +2,52 @@ using UnityEngine;
 
 public class Movimento : MonoBehaviour
 {
-    private Rigidbody2D myRigidbody;
+    private Rigidbody2D myRigidbody2D;
     private float horizontal;
+    private bool facingRight; // Virando pra direita
+    [SerializeField]
+    private float movimentoSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //Associando a variável ao componente Rigidbody2D
-        myRigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        movimentoSpeed = 10;
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        //Debug.Log(horizontal);
+    }
+    // Melhoria FixedUpdate consome menos recurso da maquina
+    void FixedUpdate()
+    {
+        HandMovimento(horizontal);
+
+        Flip(horizontal);
+    }
+    void HandMovimento(float horizontal)
+    {
+        // myRigidbody2D.velocity = Vector2.left;
+        myRigidbody2D.linearVelocity = new Vector2(horizontal * movimentoSpeed, myRigidbody2D.linearVelocity.y);
+        // Debug.Log(horizontal);
+    }
+
+    void Flip(float horizontal)
+    {
+        if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector2 theScale = transform.localScale;
+
+            // theScale.x *= theScale.x * -1;
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+            Debug.Log("O personagem virou");
+        }
     }
 }
